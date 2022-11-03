117838cc069a29029901bf9c9e19621f10b353de
GarrettA01 <115960428+GarrettA01@users.noreply.github.com>
2022-11-03 17:05:54 -0600

    Added questions variable in JS

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var questions = [
+    { question: "What does HTML stand for?",
+    A: "HyperText Markup Link",
+    B: "Hieroglyphic, To Machines Legible",
+    C: "How To Marry Linda",
+    D: "Hello Todd My Love",
+    correct: A,
+}
+
+{question: "What does CSS stand for?",
+A: "Carrie's Scary Snake",
+B: "Carrying Soggy Socks",
+C: "Cascading Style Sheets",
+D: "Calling Saul's Son",
+correct: C,
+}
+]
