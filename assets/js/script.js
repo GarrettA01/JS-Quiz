8ecc4c750bae39b70be050aee6f438327f972d5d
GarrettA01 <115960428+GarrettA01@users.noreply.github.com>
2022-11-03 19:33:29 -0600

    Added starter questions to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,24 @@ var questions = [
     B: "Hieroglyphic, To Machines Legible",
     C: "How To Marry Linda",
     D: "Hello Todd My Love",
-    correct: A,
-}
+    correct: "A",
+},
 
-{question: "What does CSS stand for?",
+{ question: "What does CSS stand for?",
 A: "Carrie's Scary Snake",
 B: "Carrying Soggy Socks",
 C: "Cascading Style Sheets",
 D: "Calling Saul's Son",
-correct: C,
+correct: "C",
 }
 ]
+
+var startButton = document.querySelector('#startbutton')
+
+startButton.addEventListener('click', startQuiz)
+
+
+function startQuiz () {
+    startButton.addSt
+
+}
